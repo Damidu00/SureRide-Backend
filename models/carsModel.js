4a146527c5fbeae2c388f3e55978ff7de1d5c2ac
Damidu00 <damidunayanajith2001@gmail.com>
2025-08-19 15:00:11 +0530

    car model created

---
+++ models/carsModel.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const carSchema = mongoose.Schema({
+    carId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  brand: {
+    type: String,
+    required: true
+  },
+  model: {
+    type: String,
+    required: true
+  },
+  year: {
+    type: Number,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  color: {
+    type: String
+  },
+  registrationNumber: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  pricePerDay: {
+    type: Number,
+    required: true
+  },
+  availability: {
+    type: Boolean,
+    default: true
+  },
+  mileage: {
+    type: Number
+  },
+  features: {
+    type: [String]
+  }
+}, { timestamps: true });
+
+export default mongoose.model("Cars", carSchema);
