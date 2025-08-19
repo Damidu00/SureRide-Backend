a88b6f12a65481be387274f4d6db23eba339d335
Damidu00 <damidunayanajith2001@gmail.com>
2025-08-19 14:46:45 +0530

    project initialized

---
+++ index.js
@@ -0,0 +1,38 @@
+import bodyParser from 'body-parser';
+import express from 'express'
+import mongoose from 'mongoose'
+import jwt from 'jsonwebtoken'
+import dotenv from "dotenv"
+
+dotenv.config()
+const app = express();
+const mongoUrl = process.env.MONGO_DB_URI
+
+mongoose.connect(mongoUrl,{})
+const connection = mongoose.connection; 
+
+connection.once("open",() => {
+    console.log("Database Connected!!");
+})
+
+app.use(bodyParser.json())
+app.use((req,res,next)=>{
+
+    const token = req.header("Authorization")?.replace("Bearer ","")
+    
+    if(token != null ){
+        jwt.verify(token,process.env.SECRET,(error,decoded)=>{
+            if(!error){
+                req.user = decoded
+            }
+        })
+    }
+
+    next()
+
+})
+
+
+app.listen(5000,() =>{
+    console.log('Server is running on port 5000')
+})
