30f968a2ca1e1e845b4bd40c714f088b451ed517
Damidu Dissanayake <154224349+Damidu00@users.noreply.github.com>
2025-08-22 20:59:23 +0530

    second nav bar created and added

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createBooking, getAllBookings, getbookingById } from '../controllers/bookingController.js';
+import { createBooking, getAllBookings, getAvailableCars, getbookingById } from '../controllers/bookingController.js';
 
 const bookingRouter = express.Router()
 
 bookingRouter.post("/",createBooking)
 bookingRouter.get("/",getAllBookings)
+bookingRouter.get('/available', getAvailableCars)
 bookingRouter.get("/:id",getbookingById)
 
 
